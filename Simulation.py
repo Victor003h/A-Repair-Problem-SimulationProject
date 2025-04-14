2213c71acd8a3108cfef1994902e9ed788df3289
VictorH <pachecovictorh003@gmail.com>
2025-04-13 20:19:48 -0400

    add documentation

--- Simulation.py
+++ Simulation.py
@@ -5,18 +5,25 @@ import math
 
 
 class WorkingMachinesSimulation():
-    def __init__(self,total_machine,spares,time_break_heap,repair_distribution_function,repair_factor,working_distribution_function,alpha,mean):
+    def __init__(self,total_machine,spares,Q,repair_cost,time_break_heap,repair_distribution_function,repair_factor,working_distribution_function,alpha,mean):
         
         self.total_machine=total_machine
         self.spares=spares
         self.repair_factor=repair_factor
         self.alpha=alpha
         self.mean=mean
+        
+        
+        self.Q=Q
+        self.profit=0
+        
+        self.repair_cost=repair_cost
         #Time Variables:
         self.time=0
         self.repair_time=math.inf
+    
         self.time_break_heap=time_break_heap
-        
+    
         self.working_distribution_function=working_distribution_function
         self.repair_distribution_function=repair_distribution_function
         # State Variable
@@ -24,8 +31,11 @@ class WorkingMachinesSimulation():
         
     def BreakDown(self):
         
+        next_event_time=heapq.heappop(self.time_break_heap)
+        dt=next_event_time-self.time
+        self.profit+=self.Q * (self.total_machine-self.spares)*dt
         
-        self.time=heapq.heappop(self.time_break_heap)
+        self.time=next_event_time
         self.machines_down+=1
         
         if self.machines_down==1:
@@ -34,16 +44,15 @@ class WorkingMachinesSimulation():
             
             t1= self.working_distribution_function(self.alpha,self.mean)
             heapq.heappush(self.time_break_heap,self.time+t1)
-            self.spares-=1
+           # self.spares-=1
             
             return False
-            
             
             
         elif self.machines_down< self.spares+1:
             t= self.working_distribution_function(self.alpha,self.mean)
             heapq.heappush(self.time_break_heap,self.time+t)
-            self.spares-=1
+            #self.spares-=1
             return False
         
         elif  self.machines_down==self.spares+1:
@@ -53,9 +62,9 @@ class WorkingMachinesSimulation():
     def Reapair(self):
         
         self.time=self.repair_time
-        
+        self.profit-=self.repair_cost
         self.machines_down-=1
-        self.spares+=1
+       # self.spares+=1
         
         if self.machines_down>0:
             t=self.repair_distribution_function()
@@ -73,8 +82,13 @@ class WorkingMachinesSimulation():
                 if end:break
             else: self.Reapair()
 
-        return self.time
+        return self.time,self.profit
 
+    def reset(self):
+        self.time=0
+        self.repair_time=math.inf
+        self.machines_down=0
+        self.profit=0
         
         
         
--- main.py
+++ main.py
@@ -13,25 +13,30 @@ def main():
     def working_function(alpha=10, mean=30):
         beta = mean / alpha
         return random.gammavariate(alpha, beta)
+    
         #return random.expovariate(lambda_op)
-    
-    c=1
-    while(c<200):
-    
-        time_break_heap=[]
-        repair_factor=1.0
-        alpha=10
-        mean=30
-        for i in range(total_machine-spares):
-            heapq.heappush(time_break_heap,working_function())
-        sim=WorkingMachinesSimulation(total_machine,spares,time_break_heap,repair_function,repair_factor,working_function,alpha,mean)
+
+        
+    time_break_heap=[]
+    repair_factor=1.0
+    alpha=10
+    mean=30
+    for i in range(total_machine-spares):
+        heapq.heappush(time_break_heap,working_function())
         
         
     
-            
-        time=sim.run()
-        print(time)
-        c+=1
+  
+    time_break_heap=[]
+    Q=1.0
+    repair_cost=10.0
+    for i in range(total_machine-spares):
+        heapq.heappush(time_break_heap,working_function())
+    
+    
+    sim=WorkingMachinesSimulation(total_machine,spares,Q,repair_cost,time_break_heap,repair_function,repair_factor,working_function,alpha,mean)
+    t=sim.run()
+    print(t)
     
 if __name__=="__main__":
     main()
